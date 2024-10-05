a00eb41febe2ccefbe996fac5d6c4e09f1fd31a1
zhemepatis <rink.gab@gmail.com>
2024-10-05 11:05:52 +0300

    added cauchy solution

--- 01-lab/exercise_03.m
+++ 01-lab/exercise_03.m
@@ -1,7 +1,21 @@
 clc, clear
 
+% solving differential equation
 syms y(x);
 equation = diff(y, x) - y/x == x * exp(x);
 solution = dsolve(equation);
 
+disp("General differential equation solution");
 disp(solution)
+
+% solving Cauchy condition
+syms C1;
+cauchy_y = 0;
+cauchy_x = 1;
+
+cauchy_condition = subs(solution, 'x', cauchy_x) == cauchy_y;
+C1 = solve(cauchy_condition, C1);
+cauchy_condition_solution = subs(solution, 'C1', C1);
+
+disp("Cauchy condition solution");
+disp(cauchy_condition_solution);
