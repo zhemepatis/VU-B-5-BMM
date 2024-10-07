0d2890360dfc3fae8dd62a6d2952092535f559fc
zhemepatis <rink.gab@gmail.com>
2024-10-07 13:01:05 +0300

    added 1st ex graphs

--- 01-lab/exercise_01.m
+++ 01-lab/exercise_01.m
@@ -6,14 +6,33 @@ syms y(x) C
 equation = x * sqrt(3+y^2) * diff(x) + 2 * y * sqrt(1+x^2) * diff(y(x)) == 0;
 solution = dsolve(equation)
 
-%% preparation for plotting
-f1 = matlabFunction(solution(1));
-f2 = matlabFunction(solution(2));
-f3 = matlabFunction(solution(3));
-f4 = matlabFunction(solution(4));
-
+%% plotting first solution
 figure(1)
 plot_prep([-25 25], [-20 20]);
+hold on;
+
+C_values = (-1:2);
+func_num = length(C_values);
+colors = lines(func_num);
+for i = (1:func_num)
+    C_value = C_values(i);
+
+    syms_func = subs(solution(1), 'C1', C_value);
+    func = matlabFunction(syms_func);
+
+    graph = fplot(func);
+    graph.Color = colors(i, :);
+    graph.LineStyle = '-';
+    graph.LineWidth = 1.8;
+    graph.DisplayName = "y(x), C_1 = " + num2str(C_value);
+end
+
+hold off
+
+%% plotting second solution
+figure(2)
+plot_prep([-25 25], [-20 20]);
+hold on;
 
 C_values = (-1:2);
 func_num = length(C_values);
@@ -21,17 +40,49 @@ colors = lines(func_num);
 
 for i = (1:func_num)
     C_value = C_values(i);
-    x_values = ((exp(C_value) + 0.1):0.1:1000);
 
-    graph = plot(x_values, f1(C_value, x_values)); hold on;
+    syms_func = subs(solution(2), 'C1', C_value);
+    func = matlabFunction(syms_func);
+
+    graph = fplot(func);
     graph.Color = colors(i, :);
     graph.LineStyle = '-';
     graph.LineWidth = 1.8;
     graph.DisplayName = "y(x), C_1 = " + num2str(C_value);
-
-    graph = plot(x_values, f2(C_value, x_values)); hold on;
-    graph.Color = colors(i, :);
-    graph.LineStyle = '-';
-    graph.LineWidth = 1.8;
-    graph.HandleVisibility = 'off';
 end
+
+hold off
+
+%% plotting third solution
+figure(3)
+plot_prep([-2 2], [-2 2]);
+hold on;
+
+func = solution(3);
+real_part = real(func);
+imag_part = imag(func);
+
+graph = plot(real_part, imag_part);
+graph.Marker = ".";
+graph.MarkerSize = 10;
+graph.Color = "#622f75";
+graph.DisplayName = "y = -3^(1/2)*1i";
+
+hold off;
+
+%% plotting third solution
+figure(4)
+plot_prep([-2 2], [-2 2]);
+hold on;
+
+func = solution(4);
+real_part = real(func);
+imag_part = imag(func);
+
+graph = plot(real_part, imag_part);
+graph.Marker = ".";
+graph.MarkerSize = 10;
+graph.Color = "#622f75";
+graph.DisplayName = "y = 3^(1/2)*1i";
+
+hold off;
