5d09f4d6d389028e5636105f790a7d15dcab6e6b
zhemepatis <rink.gab@gmail.com>
2024-10-15 08:14:23 +0300

    added initial structure for second order Runge-Kutta method

--- 02-lab/runge_kutta_2o_analysis.m
+++ 02-lab/runge_kutta_2o_analysis.m
@@ -1,6 +1,7 @@
 %% clearing old values, closing figures
 clc, clear, close all
 addpath('../utils');
+addpath('algorithms');
 
 % defining function
 f = @(t, u) exp(t) .* log(2 .* u + t) + t;
@@ -10,9 +11,9 @@ initial_condition = 1; % u(0) = 1
 % solving differential equation using standard MATLAB methods
 ode45_results = ode45(f, t_span, initial_condition);
 
-% solving differential equation using Runge-Kutta method
-rk_results1 = runge_kutta_method(f, 0.1, 1);
-rk_results2 = runge_kutta_method(f, 0.05, 1);
+% solving differential equation using 2nd order Runge-Kutta method
+rk_2o_results1 = runge_kutta_2o_method(f, 0.1, 1);
+rk_2o_results2 = runge_kutta_2o_method(f, 0.05, 1);
 
 %% plotting
 figure(1);
@@ -24,15 +25,15 @@ graph = plot(ode45_results.x, ode45_results.y);
 graph.LineStyle = '-';
 graph.LineWidth = 1.8;
 
-% plotting Runge-Kutta method results
-t_values = rk_results1(1, :);
-y_values = rk_results1(2, :);
+% plotting 2nd order Runge-Kutta method results
+t_values = rk_2o_results1(1, :);
+y_values = rk_2o_results1(2, :);
 
 graph = scatter(t_values, y_values);
 graph.Marker = 'x';
 
-t_values = rk_results2(1, :);
-y_values = rk_results2(2, :);
+t_values = rk_2o_results2(1, :);
+y_values = rk_2o_results2(2, :);
 
 graph = scatter(t_values, y_values);
 graph.Marker = 'square';
