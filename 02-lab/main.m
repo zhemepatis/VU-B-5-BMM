9dcba794d6a00219d0832de0487cc2973bcd8c55
zhemepatis <rink.gab@gmail.com>
2024-10-15 08:00:34 +0300

    added some plots

--- 02-lab/main.m
+++ 02-lab/main.m
@@ -1,8 +1,40 @@
 %% clearing old values, closing figures
 clc, clear, close all
-
 addpath('../utils');
 
-f = @(x, u) exp(x) .* log(2 .* u + x) + x;
+% defining function
+f = @(t, u) exp(t) .* log(2 .* u + t) + t;
+t_span = [0, 1];
+initial_condition = 1; % u(0) = 1
 
-results = runge_kutta_method(f, [0, 1], 0.1)
+% solving differential equation using standard MATLAB methods
+ode45_results = ode45(f, t_span, initial_condition);
+
+% solving differential equation using Runge-Kutta method
+rk_results1 = runge_kutta_method(f, 0.1, 1);
+rk_results2 = runge_kutta_method(f, 0.05, 1);
+
+%% plotting
+figure(1);
+plot_prep([-2.5, 2.5], [0, 5]);
+hold on;
+
+% plotting ode45 results
+graph = plot(ode45_results.x, ode45_results.y);
+graph.LineStyle = '-';
+graph.LineWidth = 1.8;
+
+% plotting Runge-Kutta method results
+t_values = rk_results1(1, :);
+y_values = rk_results1(2, :);
+
+graph = scatter(t_values, y_values);
+graph.Marker = 'x';
+
+t_values = rk_results2(1, :);
+y_values = rk_results2(2, :);
+
+graph = scatter(t_values, y_values);
+graph.Marker = 'square';
+
+hold off;
--- 02-lab/runge_kutta_method.m
+++ 02-lab/runge_kutta_method.m
@@ -1,21 +1,23 @@
-function selected_pairs = runge_kutta_method(f, interval, tau)
-    left_end = interval(1);
-    right_end = interval(2);
-    round_num = right_end / tau;
+function results = runge_kutta_method(f, tau, T)
+    iter_num = T / tau - 1;
 
     t_n = 0;
     y_n = 1;
 
-    selected_pairs = [];
-    for i = 0:round_num
-        k1 = f(t_n, y_n)
+    t_val = [t_n];
+    y_val = [y_n];
+    for i = 0:iter_num
+        k1 = f(t_n, y_n);
         k2 = f(t_n + tau/2, y_n + k1*tau/2);
         k3 = f(t_n + tau/2, y_n + k2*tau/2);
         k4 = f(t_n + tau, y_n + k3*tau);
 
         y_n = y_n + (k1 + 2*k2 + 2*k3 + k4)*tau/6;
-        selected_pairs = [selected_pairs; t_n, y_n];
-        
         t_n = t_n + tau;
+
+        t_val = [t_val, t_n];
+        y_val = [y_val, y_n];
     end
+
+    results = [t_val; y_val];
 end
