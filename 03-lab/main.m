018f7dfc7efb54d9a74f36eb9eee266eba11bf0b
zhemepatis <rink.gab@gmail.com>
2024-12-09 15:08:09 +0200

    some shimmie, shimmie with plot parameters

--- 03-lab/main.m
+++ 03-lab/main.m
@@ -28,11 +28,11 @@ disp(cauchy_solution);
 
 %% plotting the answer
 figure(1);
-plot_prep([-10, 10], [-7, 7]);
+plot_prep([0, 20], [-7, 7]);
 
 hold on;
 
-graph = fplot(cauchy_solution);
+graph = fplot(cauchy_solution, [0, 20]);
 graph.DisplayName = "x(t)";
 graph.LineStyle = '-';
 graph.LineWidth = 1.5;
